4ad0519996903b628314b9177732fca968adcbd5
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-29 19:32:25 -0500

    Begin seed data for Levels dataset.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+levels_list = [
+  ["Level_1", false, 5, "morning", "night"],
+  ["Level_2", false, 10, "morning", "night"],
+  ["Level_3", true, 15, "morning", "night"],
+  ["Level_4", false, 20, "morning", "night"],
+  ["Level_5", false, 25, "morning", "night"],
+  ["Level_6", true, 30, "morning", "night"]
+]
+
+levels_list.each do |name, boss, rating, created_at, updated_at|
+  Levels.create(name: name, boss: boss, rating: rating, created_at: created_at, updated_at: updated_at)
+end
