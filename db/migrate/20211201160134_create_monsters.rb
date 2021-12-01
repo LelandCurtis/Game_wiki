918b4312da07c5fbf73970a1713ee1da7031c2ac
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-01 17:08:43 -0500

    Fix bug in migration.

--- db/migrate/20211201160134_create_monsters.rb
+++ db/migrate/20211201160134_create_monsters.rb
@@ -1,7 +1,7 @@
 class CreateMonsters < ActiveRecord::Migration[5.2]
   def change
     create_table :monsters do |t|
-      t.int :level_id
+      t.integer :level_id
       t.string :name
       t.boolean :enraged
       t.integer :health
