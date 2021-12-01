e6fd298bca1f5b9b9ab3fcfe9148f1ee742eabba
Eric Chon <echon92@gmail.com>
2021-12-01 16:39:30 -0700

    created feature test for index and show for parent level

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/level', to: 'levels#index'
+  get '/levels', to: 'levels#index'
+  get '/levels/:id', to: 'levels#show'  
 end
