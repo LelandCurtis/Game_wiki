646ab3a96867b3ff65d63c805cb5b31f94fd413b
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-02 13:51:33 -0500

    Fix foreign key migration. remove 's' from parent name. Fix child tests to add parent objets and reference @parent.id.

--- db/migrate/20211202035913_add_roles_ref_to_weapons.rb
+++
@@ -1,5 +0,0 @@
-class AddRolesRefToWeapons < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :weapons, :roles, foreign_key: true
-  end
-end
--- spec/features/weapons_spec.rb
+++ spec/features/weapons_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe 'all pages related to the weapons data' do
   describe 'weapons index page' do
     before :each do
-      @weapon_1 = Weapon.create!(name: 'weapon_1', ranged_attack: true, fire_rate: 0.5, damage: 30, roles_id: 1)
-      @weapon_2 = Weapon.create!(name: 'weapon_2', ranged_attack: false, fire_rate: 0.25, damage: 15, roles_id: 1)
-      @weapon_3 = Weapon.create!(name: 'weapon_3', ranged_attack: true, fire_rate: 1.0, damage: 45, roles_id: 2)
-      @weapon_4 = Weapon.create!(name: 'weapon_4', ranged_attack: true, fire_rate: 1.5, damage: 70, roles_id: 2)
-      @weapon_5 = Weapon.create!(name: 'weapon_5', ranged_attack: false, fire_rate: 1.0, damage: 80, roles_id: 3)
-      @weapon_6 = Weapon.create!(name: 'weapon_6', ranged_attack: true, fire_rate: 0.5, damage: 20, roles_id: 3)
+      @role_1 = Role.create!(name: 'role_1', unlocked: true, health: 20)
+      @role_2 = Role.create!(name: 'role_2', unlocked: true, health: 25)
+      @role_3 = Role.create!(name: 'role_3', unlocked: false, health: 30)
+
+      @weapon_1 = Weapon.create!(name: 'weapon_1', ranged_attack: true, fire_rate: 0.5, damage: 30, role_id: @role_1.id)
+      @weapon_2 = Weapon.create!(name: 'weapon_2', ranged_attack: false, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
+      @weapon_3 = Weapon.create!(name: 'weapon_3', ranged_attack: true, fire_rate: 1.0, damage: 45, role_id: @role_2.id)
+      @weapon_4 = Weapon.create!(name: 'weapon_4', ranged_attack: true, fire_rate: 1.5, damage: 70, role_id: @role_2.id)
+      @weapon_5 = Weapon.create!(name: 'weapon_5', ranged_attack: false, fire_rate: 1.0, damage: 80, role_id: @role_3.id)
+      @weapon_6 = Weapon.create!(name: 'weapon_6', ranged_attack: true, fire_rate: 0.5, damage: 20, role_id: @role_3.id)
     end
 
     it 'exists' do
