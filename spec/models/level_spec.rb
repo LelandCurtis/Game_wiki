3113e73cd6abc6de6429e63a9f24964222b0e5c7
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-02 15:51:55 -0500

    add levels model tests.

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Level, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many :weapons}
+
+  describe 'instance methods' do
+    # before :each do
+    #   @level_1 = Level.create!(name: "Level 1", boss: false, difficulty: 1)
+    #   @level_2 = Level.create!(name: "Level 2", boss: false, difficulty: 2)
+    # end
+  end
+
 end
