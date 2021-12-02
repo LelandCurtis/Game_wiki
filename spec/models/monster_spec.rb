a4eaf1e1cc7db34fd8bbc4bd5b7410f8b2f05a83
Eric Chon <echon92@gmail.com>
2021-12-02 13:58:59 -0700

    created basic models test to check for association

--- spec/models/monster_spec.rb
+++ spec/models/monster_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Monster, type: :model do
+  it {should belong_to :level}
   # pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
+  it {should have_many :weapons}
+
+  describe 'instance methods' do
+    
+  end
   # pending "add some examples to (or delete) #{__FILE__}"
 end
