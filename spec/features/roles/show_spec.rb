32feb8a60249972905323f8e3c23c5f08cb5a3b7
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-02 16:56:16 -0500

    Add monster and roles show page tests.

---
+++ spec/features/roles/show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'roles show page' do
+  before :each do
+    @role_1 = Role.create!(name: 'role_1', unlocked: true, health: 20)
+    @role_2 = Role.create!(name: 'role_2', unlocked: true, health: 25)
+    @role_3 = Role.create!(name: 'role_3', unlocked: false, health: 30)
+  end
+
+  it 'routes properly' do
+    visit "/roles/#{@role_1.id}"
+  end
+
+  it 'has a title of the name of the Role' do
+    visit "/roles/#{@role_1.id}"
+    expect(page).to have_content("Name: #{@role_1.name}")
+  end
+
+  it 'displays all data related to that role' do
+    visit "/roles/#{@role_1.id}"
+    expect(page).to have_content("Unlcoked: #{@role_1.unlocked}")
+    expect(page).to have_content("Health: #{@role_1.health}")
+  end
+
+  it "doesn't show data related to other roles" do
+    visit "/roles/#{@role_1.id}"
+    expect(page).to_not have_content("Name: #{@role_2.name}")
+    expect(page).to_not have_content("Unlcoked: #{@role_2.unlocked}")
+    expect(page).to_not have_content("Health: #{@role_2.health}")
+  end
+end
---
+++ spec/features/weapons/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'weapons show page' do
+  before :each do
+    @role_1 = Role.create!(name: 'role_1', unlocked: true, health: 20)
+    @role_2 = Role.create!(name: 'role_2', unlocked: true, health: 25)
+    @role_3 = Role.create!(name: 'role_3', unlocked: false, health: 30)
+
+    @weapon_1 = Weapon.create!(name: 'weapon_1', ranged_attack: true, fire_rate: 0.5, damage: 30, role_id: @role_1.id)
+    @weapon_2 = Weapon.create!(name: 'weapon_2', ranged_attack: false, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
+    @weapon_3 = Weapon.create!(name: 'weapon_3', ranged_attack: true, fire_rate: 1.0, damage: 45, role_id: @role_2.id)
+    @weapon_4 = Weapon.create!(name: 'weapon_4', ranged_attack: true, fire_rate: 1.5, damage: 70, role_id: @role_2.id)
+    @weapon_5 = Weapon.create!(name: 'weapon_5', ranged_attack: false, fire_rate: 1.0, damage: 80, role_id: @role_3.id)
+    @weapon_6 = Weapon.create!(name: 'weapon_6', ranged_attack: true, fire_rate: 0.5, damage: 20, role_id: @role_3.id)
+  end
+
+  it 'routes properly' do
+    visit "/weapons/#{@weapon_1.id}"
+  end
+
+  it 'has a title of the name of the weapon' do
+    visit "/weapons/#{@weapon_1.id}"
+    expect(page).to have_content("Name: #{@weapon_1.name}")
+  end
+
+  it 'displays all data related to that weapon' do
+    visit "/weapons/#{@weapon_1.id}"
+    expect(page).to have_content("Ranged Attack?: #{@weapon_1.ranged_attack}")
+    expect(page).to have_content("Fire Rate: #{@weapon_1.fire_rate}")
+    expect(page).to have_content("Damage: #{@weapon_1.damage}")
+  end
+
+  it "doesn't show data related to other weapons" do
+    visit "/weapons/#{@weapon_1.id}"
+    expect(page).to_not have_content("Ranged Attack?: #{@weapon_2.ranged_attack}")
+    expect(page).to_not have_content("Fire Rate: #{@weapon_2.fire_rate}")
+    expect(page).to_not have_content("Damage: #{@weapon_2.damage}")
+  end
+end
