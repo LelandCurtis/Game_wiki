a138511a4f6cbef5ea61981abe1266e29799cf37
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-04 22:00:45 -0500

    Add tests for name text input and unlocked boolean selector and add html.

--- spec/features/roles/new_spec.rb
+++ spec/features/roles/new_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe 'roles#new' do
     visit '/roles/new'
     expect(page).to have_css('form')
   end
+
+  it 'has text input field for name' do
+    visit '/roles/new'
+    expect(page.find('input#name').visible?).to eq(true)
+    fill_in "name", with: 'New Role'
+    expect(page).to have_field('name', with: 'New Role')
+  end
+
+  it 'has radio input field for unlocked' do
+    visit '/roles/new'
+    expect(page.find('select#unlocked').visible?).to eq(true)
+    select "True", from: 'unlocked'
+    expect(page).to have_field('unlocked', with: 'true')
+  end
+
+  it 'has a submit button' do
+
+  end
 end
