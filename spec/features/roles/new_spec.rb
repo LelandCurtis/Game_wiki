8fc4fcc7c8f74868286f4d20bbf26a9a21cde9b9
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-05 05:31:07 -0500

    Update new_spec and new.html.erb to use rails forms helpers, not pure html.

--- spec/features/roles/new_spec.rb
+++ spec/features/roles/new_spec.rb
@@ -12,42 +12,37 @@ RSpec.describe 'roles#new' do
     expect(current_path).to eq('/roles/new')
   end
 
-  # it 'has a form' do
-  #   visit '/roles/new'
-  #   expect(page).to have_css('form')
-  # end
-  #
-  # it 'has text input field for name' do
-  #   visit '/roles/new'
-  #   expect(page).to have_field('name', with: '')
-  #   fill_in "name", with: 'New Role'
-  #   expect(page).to have_field('name', with: 'New Role')
-  # end
-  #
-  # it 'has select input field for unlocked' do
-  #   visit '/roles/new'
-  #   expect(page).to have_field('unlocked', with: 'false')
-  #   select "True", from: 'unlocked'
-  #   expect(page).to have_field('unlocked', with: 'true')
-  # end
-  #
-  # it 'has a health input as number' do
-  #   visit '/roles/new'
-  #   expect(page).to have_field('health', with: '')
-  #   fill_in "health", with: 100
-  #   expect(page).to have_field('health', with: 100)
-  # end
+  it 'has text input field for name' do
+    visit '/roles/new'
+    expect(page).to have_field('name')
+    fill_in "name", with: 'New Role'
+    expect(page).to have_field('name', with: 'New Role')
+  end
+
+  it 'has select input field for unlocked' do
+    visit '/roles/new'
+    expect(page).to have_field('unlocked')
+    select "True", from: 'unlocked'
+    expect(page).to have_field('unlocked', with: 'true')
+  end
+
+  it 'has a health input as number' do
+    visit '/roles/new'
+    expect(page).to have_field('health')
+    fill_in "health", with: 100
+    expect(page).to have_field('health', with: 100)
+  end
 
   it 'has a submit button that redirects to roles index after new role is added' do
     visit '/roles/new'
     save_and_open_page
-    expect(page).to have_button('submit')
+    expect(page).to have_button('Create Role')
 
     fill_in "name", with: 'New Role'
     select "True", from: 'unlocked'
     fill_in "health", with: 100
 
-    click_button('submit')
+    click_button('Create Role')
     role_id = Role.last.id
     expect(current_path).to eq("/roles")
     expect(page).to have_content("New Role")
