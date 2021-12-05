a5e3e00eb3420fd98fcd53b8570171bacf468757
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-05 11:40:11 -0500

    Add new weapon test for link.

---
+++ spec/features/roles/weapons/new_spec.rb
@@ -0,0 +1,27 @@
+#   As a visitor
+# When I visit a Parent Childs Index page
+# Then I see a link to add a new adoptable child for that parent "Create Child"
+# When I click the link
+# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
+# When I fill in the form with the child's attributes:
+# And I click the button "Create Child"
+# Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
+# a new child object/row is created for that parent,
+# and I am redirected to the Parent Childs Index page where I can see the new child listed
+require 'rails_helper'
+
+RSpec.describe 'weapons create' do
+  before :each do
+    @role_1 = Role.create!(name: 'role_1', unlocked: true, health: 20)
+
+    @weapon_1 = Weapon.create!(name: 'weapon_1', ranged_attack: true, fire_rate: 0.5, damage: 30, role_id: @role_1.id)
+    @weapon_2 = Weapon.create!(name: 'weapon_2', ranged_attack: false, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
+  end
+
+  it 'is linked from the roles_weapons index page' do
+    visit "/roles/#{@role_1.id}/weapons"
+    expect(page).to have_link('Create New Weapon')
+    click_link "Create New Weapon"
+    expect(current_page).to eq("/roles/#{@role_1.id}/weapons/new")
+  end
+end
