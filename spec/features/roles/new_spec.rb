eb83ef53944dabc42e4144d1acff8b7c1ea85e10
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-04 22:10:18 -0500

    Add test for submit button. Update tests to be cleaner.

--- spec/features/roles/new_spec.rb
+++ spec/features/roles/new_spec.rb
@@ -12,26 +12,27 @@ RSpec.describe 'roles#new' do
 
   it 'has text input field for name' do
     visit '/roles/new'
-    expect(page.find('input#name').visible?).to eq(true)
+    expect(page).to have_field('name', with: '')
     fill_in "name", with: 'New Role'
     expect(page).to have_field('name', with: 'New Role')
   end
 
   it 'has radio input field for unlocked' do
     visit '/roles/new'
-    expect(page.find('select#unlocked').visible?).to eq(true)
+    expect(page).to have_field('unlocked', with: 'false')
     select "True", from: 'unlocked'
     expect(page).to have_field('unlocked', with: 'true')
   end
 
   it 'has a health input as number' do
     visit '/roles/new'
-    expect(page.find('input#health').visible?).to eq(true)
+    expect(page).to have_field('health', with: '')
     fill_in "health", with: 100
     expect(page).to have_field('health', with: 100)
   end
 
   it 'has a submit button' do
-
+    visit '/roles/new'
+    expect(page).to have_field('submit')
   end
 end
