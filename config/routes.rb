334895253b09503deaf5398b3ffda652b863cc56
Eric Chon <89038271+echon006@users.noreply.github.com>
2021-12-06 14:56:55 -0700

    Revert "Story 14"

--- config/routes.rb
+++ config/routes.rb
@@ -5,27 +5,20 @@ Rails.application.routes.draw do
   get '/levels/:id', to: 'levels#show'
   post '/levels', to: 'levels#create'
   get '/levels/:level_id/edit', to: 'levels#edit'
-  patch '/levels/:level_id', to: 'levels#update'
-
+  patch 'levels/:level_id', to: 'levels#update'
   get '/monsters', to: 'monsters#index'
   get '/monsters/:id', to: 'monsters#show'
-  get '/monsters/:id/edit', to: 'monsters#edit'
-  patch '/monsters/:id', to: 'monsters#update'
-
   get '/roles', to: 'roles#index'
   get '/roles/new', to: 'roles#new'
   get '/roles/:id', to: 'roles#show'
   post '/roles', to: 'roles#create'
   get '/roles/:role_id/edit', to: 'roles#edit'
   patch '/roles/:role_id', to: 'roles#update'
-
   get '/weapons', to: 'weapons#index'
   get '/weapons/:id', to: 'weapons#show'
-
   get '/roles/:role_id/weapons', to: 'role_weapons#index'
   get '/roles/:role_id/weapons/new', to: 'role_weapons#new'
   post '/roles/:role_id/weapons', to: 'role_weapons#create'
-
   get '/levels/:level_id/monsters', to: 'level_monsters#index'
   get '/levels/:level_id/monsters/new', to: 'level_monsters#new'
   post '/levels/:level_id/monsters', to: 'level_monsters#create'
