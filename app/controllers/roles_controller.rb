3fada975add262b33f61dc6b0d7496a2a629c242
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-06 11:57:10 -0700

    Add role_params method to permit specific attrs. Update update method.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -23,7 +23,11 @@ class RolesController < ApplicationController
 
   def update
     @role = Role.find(params[:role_id])
-    Role.update(@role.id, name: params[:name], unlocked: params[:unlocked], health: params[:health])
+    @role.update(role_params)
     redirect_to "/roles/#{@role.id}"
+  end
+
+  def role_params
+    params.permit(:name, :unlocked, :health)
   end
 end
