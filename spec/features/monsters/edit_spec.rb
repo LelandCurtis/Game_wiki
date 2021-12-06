07b2a62169db9fd41ae9b0455960983aa611d6f6
Eric Chon <echon92@gmail.com>
2021-12-06 14:35:29 -0700

    completed story 14

--- spec/features/monsters/edit_spec.rb
+++ spec/features/monsters/edit_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'monsters edit' do
     expect(current_path).to eq("/monsters/#{@monster_1.id}/edit")
   end
 
-  xit "can edit the levels" do
+  it "can edit the levels" do
     visit "/monsters/#{@monster_1.id}/edit"
 
     fill_in('Name', with: "Monster_12")
