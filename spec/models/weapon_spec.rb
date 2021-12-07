2667d080249e4f292edbfa04a41e70620355a339
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-06 17:16:16 -0700

    Update test syntax. Write test for by_name.

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -8,14 +8,19 @@ RSpec.describe Weapon, type: :model do
       @role_1 = Role.create!(name: 'role_1', unlocked: true, health: 20)
       @role_2 = Role.create!(name: 'role_2', unlocked: true, health: 25)
 
-      @weapon_1 = Weapon.create!(name: 'weapon_1', ranged_attack: true, fire_rate: 0.5, damage: 30, role_id: @role_1.id)
-      @weapon_2 = Weapon.create!(name: 'weapon_2', ranged_attack: false, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
-      @weapon_3 = Weapon.create!(name: 'weapon_3', ranged_attack: true, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
+      @weapon_1 = Weapon.create!(name: 'Bazooka', ranged_attack: true, fire_rate: 0.5, damage: 30, role_id: @role_1.id)
+      @weapon_2 = Weapon.create!(name: 'AK-47', ranged_attack: false, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
+      @weapon_3 = Weapon.create!(name: 'Sword', ranged_attack: true, fire_rate: 0.25, damage: 15, role_id: @role_1.id)
     end
+
     it '.ranged_attack_weapons returns' do
       expected = [@weapon_1, @weapon_3]
-      ranged_weapons = Weapon.ranged_attack_weapons
-      expect(ranged_weapons).to eq(expected)
+      expect(Weapon.ranged_attack_weapons).to eq(expected)
+    end
+
+    it '.by_name sorts weapons by alphabetical order' do
+      expected = [@weapon_2, @weapon_1, @weapon3]
+      expect(Weapon.by_name).to eq(expected)
     end
   end
 end
