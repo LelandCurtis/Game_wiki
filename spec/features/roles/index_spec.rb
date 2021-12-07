535e420a8e34e5d5bc18d167732c745198e6dc9a
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-07 10:50:56 -0700

    Add test for adding links to each role in role index.

--- spec/features/roles/index_spec.rb
+++ spec/features/roles/index_spec.rb
@@ -51,4 +51,13 @@ RSpec.describe 'roles index page' do
     page.find_by_id("Edit_#{@role_2.id}").click
     expect(current_path).to eq("/roles/#{@role_2.id}/edit")
   end
+
+  it 'links to each show page through the title of the role' do
+    visit '/roles'
+    expect(page).to have_link("#{role_1.name}")
+    expect(page).to have_link("#{role_2.name}")
+    expect(page).to have_link("#{role_3.name}")
+    click_link "#{role_1.name}"
+    expect (current_path).to eq("/roles/#{role_1.id}")
+  end
 end
