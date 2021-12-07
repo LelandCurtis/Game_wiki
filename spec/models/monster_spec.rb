9056f85a0834b4a6c737f02f3b169e3e21842a99
Eric Chon <echon92@gmail.com>
2021-12-07 10:45:06 -0700

    created test for story 15

--- spec/models/monster_spec.rb
+++ spec/models/monster_spec.rb
@@ -3,7 +3,27 @@ require 'rails_helper'
 RSpec.describe Monster, type: :model do
   it {should belong_to :level}
 
-  describe 'instance methods' do
+  describe 'class methods' do
 
+  end
+
+  describe ' instance methods' do
+    before :each do
+      @level_1 = Level.create!(name: "Level 1", boss: false, difficulty: 1)
+
+      @monster_1 = Monster.create!(name: "Vampire", enraged: false, health: 10, level_id: @level_1.id)
+      @monster_2 = Monster.create!(name: "Monster_2", enraged: true, health: 7, level_id: @level_1.id)
+      @monster_3 = Monster.create!(name: "Monster_3", enraged: true, health: 8, level_id: @level_1.id)
+      @monster_4 = Monster.create!(name: "Monster_4", enraged: false, health: 12, level_id: @level_1.id)
+    end
+
+    it "returns the monsters that are enraged" do
+      expected = [@monster_2, @monster_3]
+      expect(@level_1.monsters.enraged).to eq(expected)
+    end
+
+    it "text" do
+
+    end
   end
 end
