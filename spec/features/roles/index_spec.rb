47361f48817b697b2eebe358437458e1a72c19fb
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-08 11:31:48 -0700

    Add tests.

--- spec/features/roles/index_spec.rb
+++ spec/features/roles/index_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe 'roles index page' do
     @role_1 = Role.create!(name: 'role_1', unlocked: true, health: 20)
     @role_2 = Role.create!(name: 'role_2', unlocked: true, health: 25)
     @role_3 = Role.create!(name: 'role_3', unlocked: false, health: 30)
+
+    @weapon_1 = Weapon.create!(name: 'Bazooka', ranged_attack: true, fire_rate: 0.5, damage: 30, role_id: @role_1.id)
+    @weapon_2 = Weapon.create!(name: 'AK-47', ranged_attack: false, fire_rate: 0.25, damage: 15, role_id: @role_2.id)
+    @weapon_3 = Weapon.create!(name: 'weapon_3', ranged_attack: true, fire_rate: 1.0, damage: 45, role_id: @role_2.id)
+    @weapon_4 = Weapon.create!(name: 'weapon_4', ranged_attack: true, fire_rate: 1.5, damage: 70, role_id: @role_2.id)
+    @weapon_5 = Weapon.create!(name: 'weapon_5', ranged_attack: false, fire_rate: 1.0, damage: 80, role_id: @role_3.id)
+    @weapon_6 = Weapon.create!(name: 'weapon_6', ranged_attack: true, fire_rate: 0.5, damage: 20, role_id: @role_3.id)
+
+    @weapons = [@weapon_1, @weapon_2, @weapon_3, @weapon_4, @weapon_5, @weapon_6]
     @roles = [@role_1, @role_2, @role_3]
   end
 
@@ -73,5 +82,13 @@ RSpec.describe 'roles index page' do
         expect(current_path).to_not have_content("#{role.name}")
       end
     end
+  end
+
+  it 'has a link to sort index by the # of children' do
+    visit '/roles'
+    expect(page).to have_link("Sort by weapon count")
+    expect(@role_1.name).to appear_before(@role_2.name)
+    click_link "Sort by weapon count"
+    expect(@role_2.name).to appear_before(@role_1.name)
   end
 end
