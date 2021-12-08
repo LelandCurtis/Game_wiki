65de77de5ffbe1c0ce5c6e69efdf4100557277d2
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-08 14:03:00 -0700

    remove unneccesary ranged_attack_weapons from role model.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,10 +1,6 @@
 class Role < ApplicationRecord
   has_many :weapons
 
-  def ranged_attacked_weapons
-    weapons.ranged_attacked_weapons
-  end
-
   def weapons_by_name
     weapons.by_name
   end
