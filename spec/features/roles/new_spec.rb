838140ffe37813dfd4a781e0f0dc328525bb1d96
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-08 14:25:46 -0700

    remove unecessary tests.

--- spec/features/roles/new_spec.rb
+++ spec/features/roles/new_spec.rb
@@ -12,28 +12,7 @@ RSpec.describe 'roles#new' do
     expect(current_path).to eq('/roles/new')
   end
 
-  it 'has text input field for name' do
-    visit '/roles/new'
-    expect(page).to have_field('name')
-    fill_in "name", with: 'New Role'
-    expect(page).to have_field('name', with: 'New Role')
-  end
-
-  it 'has select input field for unlocked' do
-    visit '/roles/new'
-    expect(page).to have_field('unlocked')
-    select "True", from: 'unlocked'
-    expect(page).to have_field('unlocked', with: 'true')
-  end
-
-  it 'has a health input as number' do
-    visit '/roles/new'
-    expect(page).to have_field('health')
-    fill_in "health", with: 100
-    expect(page).to have_field('health', with: 100)
-  end
-
-  it 'has a submit button that redirects to roles index after new role is added' do
+  it 'has a form to create a role and redirects to roles index after submit' do
     visit '/roles/new'
     expect(page).to have_button('Create Role')
 
